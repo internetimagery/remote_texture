a54f8a6c94496eee8032d6efa27fb2f77b8f8323
Jason <jason.dixon.email@gmail.com>
2015-03-21 00:56:42 +1300

    creating cache

--- __init__.py
+++ __init__.py
@@ -2,14 +2,18 @@
 
 # Modify textures to use remote destinations.
 import maya.cmds as cmds
-import re, os
+import re, os, md5
 
-def cache_file(path):
+def cache_file(dest):
 	#ensure Cache exists
 	base = os.path.dirname(os.path.realpath(__file__))
 	cache = os.path.join( base, "cache")
 	if not os.path.exists(cache):
 		os.makedirs(cache)
+	uid = md5.new(dest).hexdigest()
+	uidPath = os.path.join( cache, uid)
+	if not os.path.isfile(uidPath):
+		print "need to download file"
 
 def check_last_action():
 	# Looking for: setAttr -type "string" file1.fileTextureName "filename";
